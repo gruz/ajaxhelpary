318cf0e003c3364e43e836f9ef60ef0458190f22
Gruz <arygroup@gmail.com>
2016-08-13 19:10:14 +0300

    1.1.2 stable

--- ajaxhelpary.php
+++ ajaxhelpary.php
@@ -1,128 +1,216 @@
-<?php defined('_JEXEC') or die;
+<?php
+/**
+ * Ajax helper plugin for Gruz extensions
+ *
+ * @package    AjaxHelpAry
+ *
+ * @author     Gruz <arygroup@gmail.com>
+ *
+ * @copyright  0000 Copyleft - All rights reversed
+ *
+ * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+// No direct access
+defined('_JEXEC') or die;
 
 jimport('joomla.plugin.plugin');
-class plgAjaxAjaxhelpary extends JPlugin
-{
 
-	function onAjaxAjaxHelpAry() {
+/**
+ * Plugin class
+ *
+ * @author  Gruz <arygroup@gmail.com>
+ * @since   0.0.1
+ */
+class PlgAjaxAjaxhelpary extends JPlugin
+{
+	/**
+	 * Entrance point to ajax calls
+	 *
+	 * @return   void
+	 */
+	public function onAjaxAjaxHelpAry()
+	{
 		$jinput = JFactory::getApplication()->input;
-		$scope = $jinput->get('scope',null);
-		$serialize = $jinput->get('serialize',null);
-		if ($scope == 'this') {
-			$this->getFEURl ($serialize);
+		$scope = $jinput->get('scope', null);
+		$serialize = $jinput->get('serialize', null);
+
+		if ($scope == 'this')
+		{
+			$this->getFEURl($serialize);
+
 			return;
 		}
-
-//~ $tmp = array('unsubscribe'=>'a@a.com');
-//~ $tmp = base64_encode(serialize($tmp));
-//~ echo '<pre> Line: '.__LINE__.' '.PHP_EOL;
-//~ print_r($tmp);
-//~ echo PHP_EOL.'</pre>'.PHP_EOL;
-//~ $tmp = unserialize(base64_decode($serialize));
-//~ echo '<pre> Line: '.__LINE__.' '.PHP_EOL;
-//~ print_r($tmp);
-//~ echo PHP_EOL.'</pre>'.PHP_EOL;
-//~
-		if ($scope != 'direct') {
-			JSession::checkToken( 'get' ) or die( 'Invalid Token' );
+/*
+$tmp = array('unsubscribe'=>'a@a.com');
+$tmp = base64_encode(serialize($tmp));
+echo '<pre> Line: '.__LINE__.' '.PHP_EOL;
+print_r($tmp);
+echo PHP_EOL.'</pre>'.PHP_EOL;
+$tmp = unserialize(base64_decode($serialize));
+echo '<pre> Line: '.__LINE__.' '.PHP_EOL;
+print_r($tmp);
+echo PHP_EOL.'</pre>'.PHP_EOL;
+*/
+		if ($scope != 'direct')
+		{
+			JSession::checkToken('get') or die('Invalid Token');
 		}
 
-
 		$app = JFactory::getApplication();
-		if ( $app->isAdmin() ) {
+
+		if ($app->isAdmin())
+		{
 			$scope = 'admin';
-		} else { $scope = 'site'; }
+		}
+		else
+		{
+			$scope = 'site';
+		}
 
+		$plg_type = $jinput->get('plg_type', null);
+		$plg_name = $jinput->get('plg_name', null);
+		$function = $jinput->get('function', null);
+		$uniq = $jinput->get('uniq', null);
 
+		$limitplugins = $this->params->get('limitplugins', 1);
 
-		$plg_type = $jinput->get('plg_type',null);
-		$plg_name = $jinput->get('plg_name',null);
-		$function = $jinput->get('function',null);
-		$uniq = $jinput->get('uniq',null);
-
-		$limitplugins = $this->params->get('limitplugins',1);
-		if ($limitplugins) {
+		if ($limitplugins)
+		{
 			$params = $this->params->get('list_templates');
 			$list_templates = json_decode($params);
-			if (!empty($params) && $list_templates === NULL) { // The default joomla installation procedure doesn't store defaut params into the DB in the correct way
-				$params = str_replace("'",'"',$params);
+
+			if (!empty($params) && $list_templates === null)
+			{
+				// The default joomla installation procedure doesn't store defaut params into the DB in the correct way
+				$params = str_replace("'", '"', $params);
 				$list_templates = json_decode($params);
 			}
 
 			$isOk = false;
-			if (!empty($list_templates) && !empty($list_templates->plg_type)) {
-				foreach($list_templates->plg_type as $k=>$v) {
-					if (
-						$plg_type == $list_templates->plg_type[$k] &&
-						$plg_name == $list_templates->plg_name[$k] &&
-						$function == $list_templates->function[$k]
-					) {
-						if ($list_templates->scope[$k] == 'both' || $list_templates->scope[$k] == $scope) {
+
+			if (!empty($list_templates) && !empty($list_templates->plg_type))
+			{
+				foreach ($list_templates->plg_type as $k => $v)
+				{
+					if ($plg_type == $list_templates->plg_type[$k]
+						&& $plg_name == $list_templates->plg_name[$k]
+						&& $function == $list_templates->function[$k])
+					{
+						if ($list_templates->scope[$k] == 'both' || $list_templates->scope[$k] == $scope)
+						{
 							$isOk = true;
 							break;
 						}
 					}
 				}
 			}
-			if (!$isOk) {
-				return JError::raiseWarning(403,JText::_('JERROR_ALERTNOAUTHOR'). '##error##');
+
+			if (!$isOk)
+			{
+				die(JText::_('JERROR_ALERTNOAUTHOR') . '##error##');
 			}
 		}
 
-		//$plugin = JPluginHelper::getPlugin($plg_type, $plg_name);
+		// $plugin = JPluginHelper::getPlugin($plg_type, $plg_name);
 
-		JPluginHelper::importPlugin( $plg_type, $plg_name);
+		JPluginHelper::importPlugin($plg_type, $plg_name);
 		$dispatcher = JEventDispatcher::getInstance();
-		$results = $dispatcher->trigger( $function, array($uniq,$serialize) );
+		$results = $dispatcher->trigger($function, array($uniq, $serialize));
 
-		if (!empty($results)) {
+		if (!empty($results))
+		{
 			return $results[0];
 		}
 	}
-	/*
+
+	/**
 	 * The function to be called from AJAX to build really working SEF links from backend.
 	 *
 	 * Creates a backend user session, gets JRoute::_() and delets the session.
 	 * It's a must to fake login-logout at FE, as JRoute::_() doesn't create correct links
 	 * for e.g. content items limited to Registred if you are not logged in at FE.
-	 * */
-	function getFEURl ($serialize) {
+	 *
+	 * @param   string  $serialize  Serialized string
+	 *
+	 * @return   void
+	 */
+	public function getFEURl($serialize)
+	{
 		$app	= JFactory::getApplication();
-		if ($app->isAdmin()) { return; } // Has to work as a FE called function
-		$db		= JFactory::getDbo();
-		$user	= JFactory::getUser();
-		$jinput =  JFactory::getApplication()->input;
-		$userId = $jinput->get('userid',null);
-		if (empty($userId)) { return; }
+
+		// Has to work as a FE called function
+		if ($app->isAdmin())
+		{
+			return;
+		}
+
+
+		$jinput = JFactory::getApplication()->input;
+		$userId = $jinput->get('userid', null);
+
+		if (empty($userId))
+		{
+			return;
+		}
+
 		$url = unserialize(base64_decode($serialize));
 
-		if ($user->id == $userId) {	echo JRoute::_($url);	return;	}
+		$user	= JFactory::getUser();
 
-		//~ if ($user->id) {
-			//~ JError::raiseWarning('SOME_ERROR_CODE', JText::_('SWITCHUSER_YOU_HAVE_LOGIN_LOGOUT_FIRST'));
-			//~ return $app->redirect('index.php');
-		//~ }
+		if ($user->id == $userId)
+		{
+			echo JRoute::_($url);
 
+			return;
+		}
+
+		/*
+		if ($user->id) {
+			JError::raiseWarning('SOME_ERROR_CODE', JText::_('SWITCHUSER_YOU_HAVE_LOGIN_LOGOUT_FIRST'));
+			return $app->redirect('index.php');
+		}
+		*/
+
+		/*
 		$cookie = md5(JApplication::getHash('site'));
-		//~ $cookie = md5(JApplication::getHash('administrator'));
 
-		$backendSessionId = $jinput->cookie->get($cookie,null);
+		// ~ $cookie = md5(JApplication::getHash('administrator'));
+
+
+		$backendSessionId = $jinput->cookie->get($cookie, null);
+		*
+		$db		= JFactory::getDbo();
 
 		$query = 'SELECT userid'
 			. ' FROM #__session'
-			. ' WHERE session_id = '.$db->Quote($backendSessionId)
-			//~ . ' AND client_id = 1'
-			//~ . ' AND guest = 0'
-		;
+			. ' WHERE session_id = ' . $db->Quote($backendSessionId);
+
+			// ~ . ' AND client_id = 1'
+			// ~ . ' AND guest = 0'
+
 		$db->setQuery($query);
+
+		$user = $db->loadResut();
+		*/
 
 		$instance = JFactory::getUser($userId);
 
-		// If _getUser returned an error, then pass it back.
-		if (JError::isError($instance)) {		return; 		}
+		if (empty($instance->id))
+		{
+			return;
+		}
 
-		//Mark the user as logged in
-		$instance->set( 'guest', 0);
+		/*
+		// If _getUser returned an error, then pass it back.
+		if (JError::isError($instance))
+		{
+			return;
+		}
+		*/
+
+		// Mark the user as logged in
+		$instance->set('guest', 0);
 		$instance->set('aid', 1);
 
 		// Register the needed session variables
@@ -130,15 +218,16 @@ class plgAjaxAjaxhelpary extends JPlugin
 		$session->set('user', $instance);
 
 		// Get the session object
-		$table =  JTable::getInstance('session');
-		$table->load( $session->getId() );
+		$table = JTable::getInstance('session');
+		$table->load($session->getId());
 		$table->guest 		= $instance->get('guest');
 		$table->username 	= $instance->get('username');
 		$table->userid 		= intval($instance->get('id'));
 
-
 		$table->update();
 		echo JRoute::_($url);
 		$table->delete();
+
+		die();
 	}
 }
